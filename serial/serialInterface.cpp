85c16bc2af4335d739aba2e8422d7c629fd79d73
HaoSun-666 <2955851S@student.gla.ac.uk>
2025-04-15 00:48:15 +0100

    Add files via upload
    Based on the C++ serial communication module, using the termios interface to implement low-level serial port configuration and transmission/reception functions.

---
+++ serial/serialInterface.cpp
@@ -0,0 +1,29 @@
+#include "serialInterface.hpp"
+
+serialInterface::serialInterface() = default;
+serialInterface::~serialInterface() = default;
+
+bool serialInterface::serialOpen() {
+  printf("default serialOpen\r\n");
+  return false;
+}
+void serialInterface::serialClose() {
+  printf("default serialClose\r\n");
+  return;
+}
+
+ssize_t serialInterface::receiveData(char* buffer, ssize_t max_len) {
+  printf("default receiveData\r\n");
+  return 0;
+}
+
+ssize_t serialInterface::sendData(const char* buffer, ssize_t buffer_len) {
+  printf("default sendData\r\n");
+  return 0;
+}
+
+### --------------------------------------------------------------------
+### Implementation of the base class for the serial port interface.
+### Default behavior (print information and return null value)
+### Used as a virtual base class or interface.
+### --------------------------------------------------------------------
