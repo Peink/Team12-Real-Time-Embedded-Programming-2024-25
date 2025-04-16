3d1a5d3d9b50d1d3c8056c15d2e1e5a48e369212
Peining <114031182+Peink@users.noreply.github.com>
2025-04-16 03:00:11 +0100

    Delete the wrong push

--- mutex v0.0.2/build/arm/modules/center/release/CMakeFiles/mutex.dir/src/mutex_task_node/task_component/fingerlib.cpp.o.d
+++
@@ -1,3 +0,0 @@
-modules/center/release/CMakeFiles/mutex.dir/src/mutex_task_node/task_component/fingerlib.cpp.o: \
- /home/newcoder/project/mutex/modules/center/release/src/mutex_task_node/task_component/fingerlib.cpp \
- /usr/include/stdc-predef.h
