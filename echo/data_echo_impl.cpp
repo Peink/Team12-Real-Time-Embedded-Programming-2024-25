953586dbec577137532cce944989548c5619d324
Peining <114031182+Peink@users.noreply.github.com>
2025-04-17 14:35:03 +0100

    Update data_echo_impl.cpp
    Add the section of Interface Implementation, and some comments

--- echo/data_echo_impl.cpp
+++ echo/data_echo_impl.cpp
@@ -1,6 +1,7 @@
 #include "data_echo_impl.hpp"
 
 static struct gpiod_chip *chip;
+static echoEventInterFace echoEventInterFace_;
 static uint8_t distance = 0;
 static dataEchoImpl *dataEchoImpl_ = nullptr;
 static bool echo_trig_flag = false;
@@ -29,12 +30,28 @@ void dataEchoImpl::onInit() {
   //Release lines after setup
   gpiod_line_release(bing_gpio_.line);
   gpiod_line_release(trig_gpio_.line);
-
+  
+ // Register a callback handler for echo events
+  registerCallback(&echoEventInterFace_);
+  
  //Start a dedicated thread to repeatedly trigger and listen for echoes
   echoDataCheckThread_ =
       std::make_shared<std::thread>(&dataEchoImpl::echoDataCheckRun_, this);
 
   echoDataCheckThread_->detach();
+}
+
+void dataEchoImpl::hasEventCallback(lcy::mutex_event_with_param &e) {
+  switch (e.event_type) {
+    case KEY_PRESS_EVENT: {
+      buzzerStart();
+      usleep(500 * 1000); //500ms
+      buzzerEnd();
+    } break;
+
+    default:
+      break;
+  }
 }
 
 void dataEchoImpl::echoDataCheckRun_() {
@@ -47,12 +64,14 @@ void dataEchoImpl::echoDataCheckRun_() {
   }
 }
 
+//echo event broadcaster
 void dataEchoImpl::echoEvent(echo_event &e) {
   for (auto &cb : adsCallbackInterfaces) {
     cb->hasEvent(e);
   }
 }
 
+// GPIO control function
 void dataEchoImpl::setOutputHight(gpiod_chip *chip, gpio_t *gpio_) {
   gpio_->line = gpiod_chip_get_line(chip, gpio_->line_num);
   if (!gpio_->line) {
@@ -83,7 +102,7 @@ void dataEchoImpl::setOutputLow(gpiod_chip *chip, gpio_t *gpio_) {
     perror("Failed to request GPIO line as output");
   }
 
-  // set GPIO line to HIGH
+  // set GPIO line to LOW
   if (gpiod_line_set_value(gpio_->line, 0) < 0) {
     perror("Failed to set GPIO line to low");
   }
@@ -102,7 +121,6 @@ void dataEchoImpl::setEventListen(gpiod_chip *chip, gpio_t *gpio_) {
 }
 
 void dataEchoImpl::startEventListen(gpiod_chip *chip, gpio_t *gpio_) {
-  /* Notify event up to 20 times */
   int ret, j = 0;
   int count = 0;
   int last_num = 0;
@@ -112,6 +130,7 @@ void dataEchoImpl::startEventListen(gpiod_chip *chip, gpio_t *gpio_) {
   if (!gpio_->line) {
     printf("Get line failed\n");
   }
+  
   echo_event e;
   val = gpiod_line_get_value(gpio_->line);
   if (val < 0) {
@@ -125,6 +144,8 @@ void dataEchoImpl::startEventListen(gpiod_chip *chip, gpio_t *gpio_) {
   echoEvent(e);
 }
 
+
+//Ultrasonic Sensor Trigger and Buzzer control
 void dataEchoImpl::Trig() {
   // Create a short HIGH pulse on the trigger line
   setOutputHight(chip, &trig_gpio_);
@@ -146,4 +167,35 @@ void dataEchoImpl::buzzerEnd() {
   setOutputHight(chip, &bing_gpio_);
   gpiod_line_release(bing_gpio_.line);
   printf("down \r\n");
+}
+
+void echoEventInterFace::hasEvent(dataEchoImpl::echo_event &e) {
+  switch (e.event.event_type) {
+    case GPIOD_LINE_EVENT_FALLING_EDGE: {
+      if (echo_end_flag) {
+        echo_end_flag = false; //End of the pulse detected
+        printf("triger, distance : %d\r\n", distance);
+       
+        echo_trig_flag = false; // Reset the trigger flag so the sensor can fire again
+        
+        //Example logic to activate buzzer if too close,
+        // if (distance <= 10) {
+        //   if (dataEchoImpl_) dataEchoImpl_->buzzerStart();
+        // } else {
+        //   if (dataEchoImpl_) dataEchoImpl_->buzzerEnd();
+        // }
+        
+        distance = 0;
+        sleep(1);
+      }
+    } break;
+    case GPIOD_LINE_EVENT_RISING_EDGE: {
+      echo_end_flag = true; //Begin of the pulse
+      distance++; // Increment the distance counter
+      usleep(2);
+    } break;
+
+    default:
+      break;
+  }
 }
